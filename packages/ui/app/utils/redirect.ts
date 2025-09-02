e706747be629dc0cce5c1e4c077f1b31689e371b
thezzisu <thezzisu@gmail.com>
2025-09-02 16:17:25 +0800

    chore: fix app redirect

--- packages/ui/app/utils/redirect.ts
+++ packages/ui/app/utils/redirect.ts
@@ -1,5 +1,5 @@
-export const redirectToApp = (appId: string, href: string) => {
-  location.href = href
+export const redirectToApp = async (appId: string, href: string) => {
   // Use farewell page as a fallback when href is too slow to load
-  navigateTo({ path: '/farewell', query: { appId } })
+  await navigateTo({ path: '/farewell', query: { appId } })
+  location.href = href
 }
